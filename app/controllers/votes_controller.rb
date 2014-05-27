b563eb165b6ae93001bb4ceacbfcf62c4bdf3777
Aaron Manaloto <amanaloto@clinic-it.com>
2014-05-27 11:51:13 +0800

    Refactor votes controller, layout changes, replace session[:user_id] to current_user in views

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,29 +1,15 @@
 class VotesController < ApplicationController
   def up
-    create_vote(params[:id], params[:type])
-    @vote.update_attribute(:points, 1)
+    Vote.up_vote params[:id], params[:type], current_user
 
     @question = Question.find(params[:question_id])
   	redirect_to question_path(@question)
   end
 
   def down
-    create_vote(params[:id], params[:type])
-    @vote.update_attribute(:points, -1)
+    Vote.down_vote params[:id], params[:type], current_user
 
     @question = Question.find(params[:question_id])
   	redirect_to question_path(@question)
-  end
-
-  def create_vote(id, type)
-    @votable = type == "Question" ? Question.find(id) : type == "Answer" ? Answer.find(id) : Comment.find(id)
-
-    @vote = Vote.where(:user_id => session[:user_id], :votable_id => id, :votable_type => type).first
-    if @vote.nil?
-      @vote = Vote.create
-      @vote.user = current_user
-      @vote.votable = @votable
-      @vote.save
-    end
   end
 end
